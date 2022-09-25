5f30cb69cd740d4e2c87534be0923d55e140304b
mani1911 <mail2aj19@gmail.com>
2022-09-25 18:31:40 +0530

    basic setup

---
+++ src/components/Gallery.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div className="h-screen w-full bg-gradient-to-b from-black to-gray-800">
+      Gallery
+    </div>
+  );
+};
+
+export default Gallery;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-center p-3 text-bo font-cursive font-bold text-6xl bg-black text-white">
+      Art Gallery
+    </div>
+  );
+};
+
+export default Navbar;
